6d901956e3a376cf8c07b3f15446c58910c99803
Sofia <sofia.g.per@gmail.com>
2022-03-03 21:42:55 +0300

    fixes to /project get route

--- plugins/itpro/projects/routes.php
+++ plugins/itpro/projects/routes.php
@@ -1,18 +1,11 @@
 <?php
 
-use Itpro\Projects\Models\Project;
 use Itpro\Projects\Models\Technology;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/projects', function(){
-    $projects = Project::all();
-    $projects->load('platforms', 'technology');
-    return $projects;
-});
-
 Route::get('/technologies', function(){
     $technologies = Technology::all();
-    $technologies->load('project');
+    $technologies->load('projects', 'projects.platforms', 'projects.project_icon');
     return $technologies;
 });
 
