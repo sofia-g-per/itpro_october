247229d9d446456dc5d3c771645752919bce6e34
Sofia <sofia.g.per@gmail.com>
2022-03-25 20:41:58 +0300

    Добавлена адаптация на раздел Стадии разработки

--- themes/ITPROtheme/assets/js/stagesOfDevScroll.js
+++ themes/ITPROtheme/assets/js/stagesOfDevScroll.js
@@ -1,7 +1,23 @@
 
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.glider'), {
-        slidesToShow: 3,
+        slidesToShow: 1,
         draggable: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: 
+                {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: 
+                    {
+                        slidesToShow: 3,
+                    }
+            },
+        ]
     })
   })
