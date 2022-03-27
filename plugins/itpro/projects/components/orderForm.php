8ea8c65bad0b5f6e4d561cf788d3737fef574f6e
Sofia <sofia.g.per@gmail.com>
2022-03-27 10:59:28 +0300

    Рефакторинг OrderForm. Настроен вывод технологий в select и стили

--- plugins/itpro/projects/components/orderForm.php
+++ plugins/itpro/projects/components/orderForm.php
@@ -1,19 +1,34 @@
 <?php namespace Itpro\Projects\Components;
 
-use Cms\Classes\ComponentBase;
-use Input;
 use Mail;
-use Validator;
+use Input;
 use Redirect;
+use Validator;
+use Cms\Classes\ComponentBase;
+use Itpro\Projects\Models\Technology;
 
 class OrderForm extends ComponentBase
 {
+    public $technologies;
+
+    
     public function componentDetails()
     {
         return [
             'name' => 'Order Form',
             'descripiton' => 'Форма для заказа проекта пользователем'
         ];
+    }
+
+    protected function loadTechnologies(){
+        $technologies = Technology::all();
+        // $technologies->load('projects.platforms', 'projects.platforms.platform_icon', 'projects.project_icon');
+        return $technologies;
+    }
+
+
+    public function onRun(){
+        $this->technologies = $this->loadTechnologies();
     }
 
     public function onSend(){
