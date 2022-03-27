c427ad0b56866d8f0b4230e0fa8759ff506dc5c1
Sofia <sofia.g.per@gmail.com>
2022-03-27 21:04:04 +0300

    Добавлен файл Orderform.php

---
+++ plugins/itpro/projects/components/Orderform.php
@@ -0,0 +1,80 @@
+<?php namespace Itpro\Projects\Components;
+
+use Mail;
+use Input;
+use Redirect;
+use Validation;
+use Cms\Classes\ComponentBase;
+use Itpro\Projects\Models\Order;
+use Itpro\Projects\Models\Client;
+use Itpro\Projects\Models\Technology;
+use October\Rain\Support\Facades\Flash;
+use Illuminate\Support\Facades\Validator;
+use October\Rain\Exception\ValidationException;
+
+class Orderform extends ComponentBase
+{
+    public $technologies;
+
+    
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Orderform',
+            'descripiton' => 'Форма для заказа проекта пользователем'
+        ];
+    }
+
+    protected function loadTechnologies(){
+        $technologies = Technology::all();
+        return $technologies;
+    }
+
+
+    public function onRun(){
+        $this->technologies = $this->loadTechnologies();
+    }
+
+    public function onSend(){
+        $orderData = post();
+        $validator = Validator::make($orderData, [
+            'client_name'=>'required',
+            'email'=>'required|email',
+            'technology_id'=>'required',
+            'project_title'=>'required',
+            'file'=>'required|mimes:png,jpeg,docx,pdf',
+        ]);
+
+        if ( $validator->fails() ) {
+            throw new ValidationException($validator);
+        } else{
+
+
+        $client = new Client();
+        $client->name = request('client_name');
+        $client->email = request('email');
+        $client->save();
+
+        $order = new Order();
+        $order->title = request('project_title');
+        $order->client_id = $client->id;
+        $order->technology_id = request('technology_id');
+        $order->file = request()->file('file')->store('files/orders');
+        $order->save();
+        
+        Flash::success('Форма отправлена!');
+                // Mail::send('itpro.contact::mail.message', input vars , function($message){
+        // $message->to('info@itpro.moscow', '');
+        // $message->subject('Новый заказ!')
+        // });
+
+        // return response()->json([
+        //     "error"=>[
+        //         "message"=>"Success",
+        //     ]
+        // ]);
+        }
+
+
+    }
+}
