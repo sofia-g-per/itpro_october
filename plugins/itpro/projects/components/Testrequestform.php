e87691175f0e87f27848491cee9ace8d61632390
Sofia <sofia.g.per@gmail.com>
2022-03-28 18:33:16 +0300

    Валидация работает для формы testRequestForm. Форма orderform рендерится на странице проекта

--- plugins/itpro/projects/components/Testrequestform.php
+++ plugins/itpro/projects/components/Testrequestform.php
@@ -3,9 +3,13 @@
 use Mail;
 use Input;
 use Redirect;
-use Validator;
+use Validation;
 use Cms\Classes\ComponentBase;
+use Itpro\Projects\Models\Client;
 use Itpro\Projects\Models\Project;
+use Itpro\Projects\Models\TestRequest;
+use Illuminate\Support\Facades\Validator;
+use October\Rain\Exception\ValidationException;
 
 class Testrequestform extends ComponentBase
 {
@@ -28,7 +32,33 @@ class Testrequestform extends ComponentBase
     }
 
     public function onSend(){
-        
+        $orderData = post();
+        $validator = Validator::make($orderData, [
+            'client_name'=>'required',
+            'description'=>'required',
+            'email'=>'required|email',
+            'project_id'=>'required|int',
+        ]);
+    
+        if ( $validator->fails() ) {
+            throw new ValidationException($validator);
+
+        } else{
+
+        $client = new Client();
+        $client->name = request('client_name');
+        $client->email = request('email');
+        $client->save();
+    
+        $request = new TestRequest();
+        $request->description = request('description');
+        $request->client_id = $client->id;
+        $request->project_id = request('project_id');
+        $request->save();
+    
+        Flash::success('Форма отправлена!');
+
+        }
     }
 
     public $projects;
