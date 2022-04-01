eb9529228691552dcc39881e4ce0ea0adb3278ef
Sofia <sofia.g.per@gmail.com>
2022-04-01 14:11:43 +0300

    Создан второй config_list для менеджеров

--- plugins/itpro/projects/controllers/Orders.php
+++ plugins/itpro/projects/controllers/Orders.php
@@ -17,13 +17,18 @@ class Orders extends Controller
     public function listExtendQuery($query, $definition)
     {
         $user = BackendAuth::getUser();
-        if ($user->hasPermission('itpro.projects.assign_self_orders')) {
+        // dd($user->hasAccess('itpro.projects.assign_self_orders'));
+        if (BackendAuth::getUser()->hasAccess('itpro.projects.assign_self_orders')) {
             $query->where('manager_id', null)->orWhere('manager_id', $user->id);
         }
     }
 
     public function __construct()
     {
+        $user = BackendAuth::getUser();
+        if ($user->hasAccess('itpro.projects.assign_self_orders')) {
+            $this->listConfig = 'manager_config_list.yaml';
+        }
         parent::__construct();
         BackendMenu::setContext('Itpro.Projects', 'orders');
 
@@ -32,7 +37,21 @@ class Orders extends Controller
     public function onRecordClick()
     {     
         $this->vars['recordId'] = post('record_id');
-        $this->vars['order'] = Order::find(post('record_id'));
-        return $this->makePartial('order_details');
+        $order = Order::find(post('record_id'));
+        $this->vars['order'] = $order;
+        // если авторизированный пользователь имеет permissions (assign_self_orders) 
+        // (присуще менеджерам)
+        $user = BackendAuth::getUser();
+        return $this->makePartial('assign_self');
+
+        if($user->hasPermission('itpro.projects.assign_self_orders')){
+            if($order->manager_id === null){
+
+            }
+            else if($order->manager_id === $user->id){
+                // return $this->makePartial('assign_self');
+            }
+
+        }
     }
 }
