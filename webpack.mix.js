f9d20f059cc706a0a8e6c16ca7fac27b575c8de8
Sofia <sofia.g.per@gmail.com>
2022-04-01 21:18:15 +0300

    Фикс: убрано дублирование волны с этапов разработки на проекты

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
-mix.setPublicPath('./themes/ITPROtheme/assets/dist/')
+mix
 .js('./themes/ITPROtheme/assets/js/*.js', 'js/combined.js')
 .styles([
     './themes/ITPROtheme/assets/css/*.css', 
@@ -8,4 +8,5 @@ mix.setPublicPath('./themes/ITPROtheme/assets/dist/')
     './themes/ITPROtheme/assets/css/pages/*.css',
     './themes/ITPROtheme/assets/css/UI/*.css'
     ], 'css/combined.css'
-);
+)
+.setPublicPath('./themes/ITPROtheme/assets/dist/');
