79f4478127fa9568fd9c988d3c01fba71787268d
Sofia <sofia.g.per@gmail.com>
2022-04-01 12:59:23 +0300

    Для менеджеров выводятся только заказы без менеджеров или заказы этого менеджера

--- plugins/itpro/projects/controllers/Orders.php
+++ plugins/itpro/projects/controllers/Orders.php
@@ -2,21 +2,31 @@
 
 use BackendMenu;
 use Backend\Classes\Controller;
+use Backend\Facades\BackendAuth;
 use Itpro\Projects\Models\Order;
 use October\Rain\Support\Facades\Event;
 
 class Orders extends Controller
 {
-    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ];
+    public $implement = [ 'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ];
     
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
 
+    public function listExtendQuery($query, $definition)
+    {
+        $user = BackendAuth::getUser();
+        if ($user->hasPermission('itpro.projects.assign_self_orders')) {
+            $query->where('manager_id', null)->orWhere('manager_id', $user->id);
+        }
+    }
+
     public function __construct()
     {
         parent::__construct();
         BackendMenu::setContext('Itpro.Projects', 'orders');
+
     }
 
     public function onRecordClick()
