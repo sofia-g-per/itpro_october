f9fc9b5286e8837622575a5027b6bc17b4a119a0
Sofia <sofia.g.per@gmail.com>
2022-04-30 14:41:43 +0300

    Рабоает glide

--- themes/ITPROtheme/assets/js/logoScroller.js
+++ themes/ITPROtheme/assets/js/logoScroller.js
@@ -1,11 +1,26 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.logo-glider'), {
-        rewind: true, 
-        slidesToShow: 'auto',
-        slidesToScroll: 'auto',
-        itemWidth: 100,
-        draggable: true,
+// window.addEventListener('load', function(){
+//     new Glider(document.querySelector('.logo-glider'), {
+//         rewind: true, 
+//         slidesToShow: 'auto',
+//         slidesToScroll: 'auto',
+//         itemWidth: 100,
+//         draggable: true,
 
-    }).scrollItem(32);
+//     }).scrollItem(32);
 
-  })
+//   })
+// import Glide from '@glidejs/glide'
+
+// new Glide('.glide',{
+//     type: 'carousel',
+//     startAt: 0,
+//     perView: 4,
+//     rewind: true,
+//     // breakpoints:{
+//     //     4000: {
+//     //         perView: 12
+//     //     }
+        
+//     // }
+// }).mount()
+
