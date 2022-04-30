1b109b255edce260801a2fcc667949a3cb4634c8
Sofia <sofia.g.per@gmail.com>
2022-04-30 15:45:00 +0300

    Деплой: поправлен путь до картинке на главной странице. Проверены шрифты на мобильном меню

--- themes/ITPROtheme/assets/dist/js/combined.js
+++ themes/ITPROtheme/assets/dist/js/combined.js
@@ -6422,6 +6422,7 @@ document.addEventListener('DOMContentLoaded', function () {
   new Splide('.splide', {
     type: 'loop',
     drag: 'free',
+    whee: true,
     fixedWidth: '90px',
     gap: '90px',
     pagination: false,
